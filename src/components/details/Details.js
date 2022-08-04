a1380863c89e205dd787afdbeb19e8c720abb5ee
c3c1l1a <cesswangumukima@gmail.com>
2022-08-05 01:04:17 +0800

    Add chart to details route

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import CoinItem from '../coinItem/CoinChart';
 
 const Details = ({ coinId }) => {
-  console.log(coinId);
+  console.log(coinId.length);
   return (
     <div className="details">
-      {coinId}
+      {coinId.length > 0
+        ? <CoinItem coinId={coinId} />
+        : ''}
     </div>
   );
 };
