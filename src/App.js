1cc90b8ee2e6d4bc30c3a0403ac58e70868d8c60
c3c1l1a <cesswangumukima@gmail.com>
2022-08-06 01:39:31 +0800

    Add nav styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import Banner from './components/banner/Banner';
 import Details from './components/details/Details';
 import { fetchAllCoins } from './redux/coins';
+import './App.css';
 
 function App() {
   const dispatch = useDispatch();
@@ -20,7 +21,11 @@ function App() {
 
   return (
     <div className="App">
-      <NavLink onClick={onClick} to="/">Home</NavLink>
+      <NavLink className="home-nav" claonClick={onClick} to="/">
+        <svg className="back-button-icon" xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+          <path d="M20 44 0 24 20 4l2.8 2.85L5.65 24 22.8 41.15Z" />
+        </svg>
+      </NavLink>
       <Banner />
       <Routes>
         <Route exact path="/" element={<Home setCoinId={setCoinId} />} />
